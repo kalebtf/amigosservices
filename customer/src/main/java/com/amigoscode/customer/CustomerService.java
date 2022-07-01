5d888ce0d2f78acf95b9825e027cf558b3ea74ba
kaleb tisnado <kalebtf@hotmail.com>
2022-07-01 16:45:14 -0500

    Microservice Communication via HTTP

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,9 +1,16 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.fraud.FraudCheckResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -13,7 +20,21 @@ public record CustomerService(CustomerRepository customerRepository) {
 
         //todo: check if email valid
         //todo: check if email not taken
+        customerRepository.saveAndFlush(customer);
 
-        customerRepository.save(customer);
+        //todo: check if fraudster
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+
+        );
+
+        if(fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
+        //todo: send notification
+
     }
 }
